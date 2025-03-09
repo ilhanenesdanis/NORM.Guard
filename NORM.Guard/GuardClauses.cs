7d200fc800c29418e8c13367fb09795c3cd2cfde
İlhan Enes Daniş <danisenes3@gmail.com>
2025-03-09 15:14:40 +0300

    GuardClauses init

---
+++ NORM.Guard/GuardClauses.cs
@@ -0,0 +1,39 @@
+﻿namespace NORM.Guard;
+
+public static class GuardClauses
+{
+    public static void AgainstNull<T, TException>(T value, string parameter) where TException : Exception, new()
+    {
+        if (value == null)
+            ThrowException<TException>(parameter);
+    }
+    public static void AgainstNull<T>(T value, string parameter)
+    {
+        if (value == null)
+            throw new ArgumentNullException(parameter);
+    }
+    public static void AgainstInvalidString<TException>(string value, string parameter) where TException : Exception, new()
+    {
+        if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+            ThrowException<TException>(parameter);
+    }
+    public static void AgainstInvalidString(string value, string parameter)
+    {
+        if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+            throw new ArgumentException(parameter);
+    }
+    public static void Against<TException>(bool condition, string parameter) where TException : Exception, new()
+    {
+        if (condition)
+            ThrowException<TException>(parameter);
+    }
+    private static void ThrowException<TException>(string parameter) where TException : Exception, new()
+    {
+        var exception = Activator.CreateInstance(typeof(TException), parameter) as TException;
+        if (exception == null)
+            throw new ArgumentException(parameter);
+        throw exception;
+    }
+
+
+}
